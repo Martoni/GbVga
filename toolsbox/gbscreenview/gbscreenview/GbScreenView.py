4bf4c23a36b13218abbd6225012210dd8b453d86
Fabien Marteau <fabien.marteau@armadeus.com>
2020-09-27 22:12:08 +0200

    GbScreenView: show first image in csv traces

--- toolsbox/gbscreenview/gbscreenview/GbScreenView.py
+++ toolsbox/gbscreenview/gbscreenview/GbScreenView.py
@@ -1,10 +1,71 @@
 import sys
+import csv
 import getopt
 import serial
 import logging
 from PIL import Image, ImageDraw
 
 class GbScreenView(object):
-    
+    # Color from wikipedia  Game_Boy page
+    # Green
+    COLOR = [
+            "#0F380F",
+            "#8BAC0F",
+            "#306230",
+            "#9BBC0F"]
+    # Black
+    #COLOR = ["#000000", "#555555", "#AAAAAA", "#FFFFFF"]
+
     def __init__(self):
-        pass
+        self.image = []
+
+    def _wait_vsync(self, freader):
+        # Find rising edge of vsync
+        for curtime, hsync, d1, clk, d0, vsync in freader:
+            if int(vsync) == 1:
+                return
+
+    def _wait_hsync(self, freader):
+        # Find falling edge of hsync
+        for curtime, hsync, d1, clk, d0, vsync in freader:
+            if int(hsync) == 1:
+                return
+
+    def _wait_clk_fall(self, freader):
+        old_clk = 0
+        for curtime, hsync, d1, clk, d0, vsync in freader:
+            if int(clk) == 0 and int(old_clk) == 1:
+                strvalue = f"{d1.strip()}{d0.strip()}"
+                return strvalue
+            old_clk = clk
+
+    def read_csv(self, filename):
+        """ Read first image """
+        with open(filename) as csvfile:
+            freader = csv.reader(csvfile, delimiter=',')
+            print("titles")
+            print(next(freader))
+            vsync_old = 0
+            hsync_old = 0
+            self._wait_vsync(freader)
+            for j in range(140):
+                self._wait_hsync(freader)
+                line = []
+                for i in range(160):
+                    line.append(self._wait_clk_fall(freader))
+                self.image.append(line)
+
+    def show(self):
+        if self.image == []:
+            raise Exception("Read data first")
+        square = 3 
+        im = Image.new("RGB", (160*square, 140*square), "#000000")
+        d = ImageDraw.Draw(im)
+        color = list(self.COLOR)
+        color.reverse()
+        for j in range(140):
+            for i in range(160):
+                d.rectangle([(i*square, j*square),
+                             (i*square+(square-1), j*square+(square-1))],
+                             fill=color[int(self.image[j][i], 2)])
+        im.show()
--- toolsbox/gbscreenview/gbscreenview/__init__.py
+++ toolsbox/gbscreenview/gbscreenview/__init__.py
@@ -2,6 +2,7 @@ import sys
 import getopt
 import serial
 import logging
+from .GbScreenView import GbScreenView
 
 import pkg_resources  # part of setuptools
 __version__ = pkg_resources.require('gbscreenview')[0].version
@@ -11,17 +12,24 @@ def usages():
     print("Usages:")
     print("gbscreenview [options]")
     print("-h, --help               print this help")
+    print("-c, --csv filename.csv   give csv filename")
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "h", ["help"])
+        opts, args = getopt.getopt(argv, "hc:",
+                ["help", "csv="])
     except getopt.GetoptError:
         usages()
         sys.exit(2)
 
+    csv = None
     for opt, arg in opts:
         if opt in ["-h", "--help"]:
             usages()
             sys.exit(0)
+        elif opt in ["-c", "--csv"]:
+            csv = arg
 
-    print("TODO")
+    gsv = GbScreenView()
+    gsv.read_csv(csv)
+    gsv.show()
