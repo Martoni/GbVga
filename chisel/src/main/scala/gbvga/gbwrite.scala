5fb6f87a4d2d846792ef61204d4a67494f28b0f3
Fabien Marteau <fabien.marteau@armadeus.com>
2020-10-01 22:10:33 +0200

    chisel: failing to add formal

--- chisel/src/main/scala/gbvga/gbwrite.scala
+++ chisel/src/main/scala/gbvga/gbwrite.scala
@@ -2,7 +2,9 @@ package gbvga
 
 import chisel3._
 import chisel3.util._
+//import chisel3.formal._
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+//import chisel3.experimental.{verification => formal}
 
 import GbConst._
 
