4107c09f267bd267dcd4b0f3f464023fc7f44449
Fabien Marteau <fabien.marteau@armadeus.com>
2020-10-17 08:05:31 +0200

    hvsync: cocotb running test

--- chisel/src/main/scala/gbvga/hvsync.scala
+++ chisel/src/main/scala/gbvga/hvsync.scala
@@ -13,35 +13,62 @@ import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 
 import GbConst._
 
-object VGAConst {
-/* Dimensions for 640x480 @ 60Hz
-* found here http://martin.hinner.info/vga/640x480_60.html*/
-
-  val H_DISPLAY = 640  // horizontal display width
-  val H_FRONT = 8      // front porch
-  val H_SYNC = 96      // sync width
-  val H_BACK = 40      // back porch
-  val
-  val V_SYNC = 4       // sync width
-  val V_BACK = 25      // back porch
-  val V_TOP = 4        // top border
-  val V_DISPLAY = 480  // vertical display width
-  val V_BOTTOM = 14    // bottom border
-  val H_SYNC_START = H_DISPLAY + H_FRONT
-  val H_SYNC_END = H_DISPLAY + H_FRONT + H_SYNC - 1
-  val H_MAX = H_DISPLAY + H_BACK + H_FRONT + H_SYNC - 1
-  val V_SYNC_START = V_DISPLAY + V_BOTTOM
-  val V_SYNC_END = V_DISPLAY + V_BOTTOM + V_SYNC - 1
-  val V_MAX = V_DISPLAY + V_TOP + V_BOTTOM + V_SYNC - 1
-}
-
-class HVSync extends Module with Formal {
+class HVSync extends Module { // with Formal { scala version problem
   val io = IO(new Bundle {
-     val hsync = Output(Bool()) 
+     val hsync = Output(Bool())
      val vsync = Output(Bool())
      val display_on = Output(Bool())
      val hpos = Output(UInt(10.W))
      val vpos = Output(UInt(9.W))
   })
 
+  val H_DISPLAY = 640.U  // horizontal display width
+  val H_FRONT = 8.U      // front porch
+  val H_SYNC = 96.U      // sync width
+  val H_BACK = 40.U      // back porch
+  val V_SYNC = 4.U       // sync width
+  val V_BACK = 25.U      // back porch
+  val V_TOP = 4.U        // top border
+  val V_DISPLAY = 480.U  // vertical display width
+  val V_BOTTOM = 14.U    // bottom border
+  val H_SYNC_START = H_DISPLAY + H_FRONT
+  val H_SYNC_END = H_DISPLAY + H_FRONT + H_SYNC - 1.U
+  val H_MAX = H_DISPLAY + H_BACK + H_FRONT + H_SYNC - 1.U
+  val V_SYNC_START = V_DISPLAY + V_BOTTOM
+  val V_SYNC_END = V_DISPLAY + V_BOTTOM + V_SYNC - 1.U
+  val V_MAX = V_DISPLAY + V_TOP + V_BOTTOM + V_SYNC - 1.U
+
+  val vpos_count = RegInit(0.U(9.W))
+  val hpos_count = RegInit(0.U(10.W))
+  io.vpos := vpos_count
+  io.hpos := hpos_count
+
+  io.display_on := (hpos_count < H_DISPLAY) && (vpos_count < V_DISPLAY)
+
+  /* Horizontal counter */
+  io.hsync := !((hpos_count >= H_SYNC_START) &&
+                (hpos_count <= H_SYNC_END))
+
+  val hpos_max = hpos_count === H_MAX
+  val vpos_max = vpos_count === V_MAX
+
+  hpos_count := hpos_count + 1.U
+  when(hpos_max){
+    hpos_count := 0.U
+  }
+
+  /* Vertical counter */
+  io.vsync := !((vpos_count >= V_SYNC_START) &&
+              (vpos_count <= V_SYNC_END))
+  when(hpos_max) {
+    vpos_count := vpos_count + 1.U
+    when(vpos_max) {
+      vpos_count := 0.U
+    }
+  }
+}
+
+object HVSyncDriver extends App {
+  (new ChiselStage).execute(args,
+    Seq(ChiselGeneratorAnnotation(() => new HVSync())))
 }
--- cocotb/test_hvsync/test_HVSync.py
+++ cocotb/test_hvsync/test_HVSync.py
@@ -17,14 +17,9 @@ from cocotb.result import TestFailure
 from cocotb.triggers import RisingEdge
 from cocotb.triggers import FallingEdge
 from cocotb.triggers import ClockCycles
-from gbscreenview import GbScreenView
-from image_test import mem_image
 
-class TestGbWrite(object):
-    #CLK_PER = (40, "ns") #25Mhz
-    CLK_PER = (80, "ns") #12.5Mhz
-    #CLK_PER = (100, "ns") #10Mhz
-    CSV_FILENAME = "../../assets/screenshoot/beautyandbeast.csv"
+class TestHVSync(object):
+    CLK_PER = (40, "ns") #25Mhz
 
     def __init__(self, dut):
         self._dut = dut
@@ -33,10 +28,8 @@ class TestGbWrite(object):
         self.rst = dut.reset
         self._clock_thread = cocotb.fork(
                 Clock(self.clk, *self.CLK_PER).start())
-        self._gsv = GbScreenView()
         self._td = cocotb.fork(
                 self.time_display(step=(1, "ms")))
-        self._mem = {}
 
     @classmethod
     def freq(cls, clkper):
@@ -57,61 +50,18 @@ class TestGbWrite(object):
             await Timer(*step)
             dtime += 1
 
-
-    async def memory_writer(self):
-        while True:
-            await RisingEdge(self._dut.clock)
-            if self._dut.io_Mwrite.value.integer > 0:
-                addr  = self._dut.io_Maddr.value.integer
-                data = self._dut.io_Mdata.value.integer
-                self._mem[addr] = data
-
-    def display_memory(self):
-        self._gsv.mem_2_image(self._mem)
-        self._gsv.show()
-
     async def reset(self):
         self.rst <= 1
         await Timer(100, units="ns")
         self.rst <= 0
-        self._mem_writer = cocotb.fork(self.memory_writer())
-        self._gbsigs = cocotb.fork(self._gsv.gen_waves(
-                        self._dut.io_GBHsync,
-                        self._dut.io_GBVsync,
-                        self._dut.io_GBClk,
-                        self._dut.io_GBData,
-                        self.log,
-                        self.CSV_FILENAME))
-
         await RisingEdge(self.clk)
 
 @cocotb.test()
 async def one_frame(dut):
     fname = "one_frame"
-    tgw = TestGbWrite(dut)
-    tgw.display_config()
+    thv = TestHVSync(dut)
+    thv.display_config()
     await Timer(1)
-    tgw.log.info("Running test {}".format(fname))
-    await tgw.reset()
-
-    # Beginning of first full frame
-    await RisingEdge(dut.io_GBVsync)
-    tgw.log.info("GBVsync rise")
-    await FallingEdge(dut.io_GBVsync)
-    tgw.log.info("GBVsync fall")
-
-    # Beginning of second frame
-    await RisingEdge(dut.io_GBVsync)
-    tgw.display_memory()
-
-    pixelcount = dut.io_countcol.value.integer
-    rightpixelcount = 0x5A00
-    if pixelcount != rightpixelcount:
-        msg = ("Wrong value of pixel count {}, should be {}"
-                .format(pixelcount, rightpixelcount))
-        tgw.log.error(msg)
-        raise TestFailure(msg)
-    tgw.log.info("GBVsync rise")
-    await FallingEdge(dut.io_GBVsync)
-    tgw.log.info("GBVsync fall")
-    tgw.log.info("End of {} test".format(fname))
+    thv.log.info("Running test {}".format(fname))
+    await thv.reset()
+    await FallingEdge(dut.io_vsync)
