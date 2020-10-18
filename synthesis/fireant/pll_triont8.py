5526bfb5691a67327068c0e518298d84329b4c2d
Fabien Marteau <fabien.marteau@armadeus.com>
2020-10-18 10:54:31 +0200

    synthesis: pll calculation nitpick

--- synthesis/fireant/pll_triont8.py
+++ synthesis/fireant/pll_triont8.py
@@ -1,11 +1,11 @@
 
-N = [v+1 for v in range(15)]                                                                                                                                                                              
-M = [v+1 for v in range(255)]                                                                                                                                                                             
-O = [1,2,4,8]                                                                                                                                                                                             
-Od = [2,4,8,16,32,64,128,256]                                                                                                                                                                             
+N = [float(v+1) for v in range(15)]
+M = [float(v+1) for v in range(255)]
+O = [float(v) for v in [1,2,4,8]]
+Od = [float(v) for v in [2,4,8,16,32,64,128,256]]
 Fin = 100/3.
 
-Tolerance = 0.1
+Tolerance = 0.01
 Ftarget = 25.0
 
 for vOd in Od:
@@ -13,9 +13,10 @@ for vOd in Od:
         for vM in M:
             for vN in N:
                 Fpfd = Fin/vN
-                if Fpfd >= 10.0 and Fpfd <= 50.0:
+                if (Fpfd >= 10.0) and (Fpfd <= 50.0):
                     Fvco = Fpfd*vM
-                    if Fvco >= 500 and Fvco <= 1500:
+                    if (Fvco >= 500.0) and (Fvco <= 1500.0):
                         fclkout = (Fvco/vO)/vOd
-                        if (fclkout <= Ftarget+Tolerance) and (fclkout >= Ftarget-Tolerance):
-                            print("Ftarget {} found : N{} M{} O{} Od{} [Fpfd {}, Fvco {}]".format(fclkout, vN,vM,vO,vOd, Fpfd, Fvco))
+                        if (fclkout <= (Ftarget + Tolerance)) and (fclkout >= (Ftarget - Tolerance)):
+                            print("Ftarget {} found : N{} M{} O{} Od{} [Fpfd {}, Fvco {}]"
+                                    .format(fclkout, vN, vM, vO, vOd, Fpfd, Fvco))
