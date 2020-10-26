a2f1e45a23a4e1bcb7c6a35ccb23719dadf355b3
Fabien Marteau <fabien.marteau@armadeus.com>
2020-10-26 11:43:23 +0100

    topgbvga: adding final top component

--- chisel/src/main/scala/gbvga/gbvga.scala
+++ chisel/src/main/scala/gbvga/gbvga.scala
@@ -3,15 +3,27 @@ package gbvga
 import chisel3._
 import chisel3.util._
 
-class GbVga extends Module {
-  val io = IO(new Bundle {
-  // Input Game boy signals
-   /* GameBoy input */
-    val GBHsync    = Input(Bool())
-    val GBVsync    = Input(Bool())
-    val GBClk      = Input(Bool())
-    val GBData     = Input(UInt(2.W))
 
-  // output VGA
+class GbVga extends Module {
+  val io = IO({
+    /* Game boy input signals */
+    val gb_hsync = Input(Bool())
+    val gb_vsync = Input(Bool())
+    val gb_clk = Input(Bool())
+    val gb_data = Input(Bool())
+    /* VGA output signals */
+    val vga_hsync = Output(Bool())
+    val vga_vsync = Output(Bool())
+    val vga_red = Output(Bool())
+    val vga_green = Output(Bool())
+    val vga_blue = Output(Bool())
   })
+
+
+
+}
+
+object GbVgaDriver extends App {
+  (new ChiselStage).execute(args,
+    Seq(ChiselGeneratorAnnotation(() => new GbVga())))
 }
---
+++ chisel/src/main/scala/gbvga/topgbvga.scala
@@ -0,0 +1,57 @@
+package gbvga
+
+import chisel3._
+import chisel3.util._
+
+
+class TopGbVga extends RawModule {
+
+  /************/
+  /** outputs */
+  /* Clock and reset */
+  val clock = IO(Input(Clock()))
+  val resetn = IO(Input(Bool()))
+  val pll_rstn = IO(Output(Bool()))
+
+  /* game boy signals */
+  val gb_hsync = IO(Input(Bool()))
+  val gb_vsync = IO(Input(Bool()))
+  val gb_clk = IO(Input(Bool()))
+  val gb_data = IO(Input(UInt(2.W)))
+
+  /* Vga */
+  val hsync = IO(Output(Bool()))
+  val vsync = IO(Output(Bool()))
+  val red = IO(Output(UInt(6.W)))
+  val green = IO(Output(UInt(6.W)))
+  val blue = IO(Output(UInt(6.W)))
+
+  withClockAndReset(clock, ~resetn) {
+    /* Activate pll at start*/
+    pll_rstn := true.B
+   
+    /* synchronize gameboy input signals with clock */
+    val shsync = ShiftRegister(gb_hsync,2)
+    val svsync = ShiftRegister(gb_vsync,2)
+    val sclk   = ShiftRegister(gb_clk  ,2)
+    val sdata  = ShiftRegister(gb_data ,2)
+
+    /* top GbVga module instantiation */
+    gbVga = Module(new GbVga(2))
+    gbVga.io.gb_hsync := shsync
+    gbVga.io.gb_vsync := svsync
+    gbVga.io.gb_clk   := sclk
+    gbVga.io.gb_data  := sdata
+    hsync := gbVga.io.vga_hsync
+    vsync := gbVga.io.vga_vsync
+    red   := gbVga.io.vga_red
+    green := gbVga.io.vga_green
+    blue  := gbVga.io.vga_blue
+
+  }
+}
+
+object TopGbVgaDriver extends App {
+  (new ChiselStage).execute(args,
+    Seq(ChiselGeneratorAnnotation(() => new TopGbVga())))
+}
